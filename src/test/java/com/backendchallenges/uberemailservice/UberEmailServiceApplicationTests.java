b71f2b3d3fc2df6f287ce6d6e42158c508223c0f
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2023-09-30 21:45:36 -0300

    feat: Project root structure

---
+++ src/test/java/com/backendchallenges/uberemailservice/UberEmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backendchallenges.uberemailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberEmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
