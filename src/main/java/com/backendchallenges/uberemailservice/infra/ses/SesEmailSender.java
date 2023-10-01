02ae3da0936f2ea49d0c23ab77c2a23d051bdaa2
Gabriel_Jales <gabrielnascimentojaless@gmail.com>
2023-09-30 21:42:57 -0300

    feat: Create SesEmailSender

---
+++ src/main/java/com/backendchallenges/uberemailservice/infra/ses/SesEmailSender.java
@@ -0,0 +1,37 @@
+package com.backendchallenges.uberemailservice.infra.ses;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.model.*;
+import com.backendchallenges.uberemailservice.adapters.EmailSenderGateway;
+import com.backendchallenges.uberemailservice.core.exceptions.EmailServiceException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SesEmailSender implements EmailSenderGateway {
+
+    private final AmazonSimpleEmailService amazonSimpleEmailService;
+
+    @Autowired
+    public SesEmailSender(AmazonSimpleEmailService amazonSimpleEmailService) {
+        this.amazonSimpleEmailService = amazonSimpleEmailService;
+    }
+
+    @Override
+    public void sendEmail(String to, String subject, String body) {
+        SendEmailRequest request = new SendEmailRequest()
+                .withSource("gabrielnascimentojaless@gmail.com")
+                .withDestination(new Destination().withToAddresses(to))
+                .withMessage(new Message()
+                        .withSubject(new Content(subject))
+                        .withBody(new Body().withText(new Content(body))) // Caso fosse necessário passar um template HTML: ... .withText(new Content(body)).withHtml()
+                );
+
+        try {
+            this.amazonSimpleEmailService.sendEmail(request);
+        } catch (AmazonServiceException exception) {
+            throw new EmailServiceException("Failure while sending email", exception);
+        }
+    }
+}
